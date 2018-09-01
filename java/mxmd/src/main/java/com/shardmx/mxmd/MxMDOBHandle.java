73b29bcf421efb313b9535abf18080d95796d94f
Huw Rogers <hrogers@fusionsystems.org>
2018-09-02 02:05:06 +0800

    JNI work

--- java/mxmd/src/main/java/com/shardmx/mxmd/MxMDOBHandle.java
+++ java/mxmd/src/main/java/com/shardmx/mxmd/MxMDOBHandle.java
@@ -2,6 +2,7 @@ package com.shardmx.mxmd;
 
 public class MxMDOBHandle implements AutoCloseable {
   private MxMDOBHandle(long ptr) { ctor_(this.ptr = ptr); }
+  public void finalize() { close(); }
   public void close() { dtor_(this.ptr); this.ptr = 0; }
 
   private native void ctor_(long ptr);
--- java/mxmd/src/main/java/com/shardmx/mxmd/MxMDSecHandle.java
+++ java/mxmd/src/main/java/com/shardmx/mxmd/MxMDSecHandle.java
@@ -2,6 +2,7 @@ package com.shardmx.mxmd;
 
 public class MxMDSecHandle implements AutoCloseable {
   private MxMDSecHandle(long ptr) { ctor_(this.ptr = ptr); }
+  public void finalize() { close(); }
   public void close() { dtor_(this.ptr); this.ptr = 0; }
 
   private native void ctor_(long ptr);

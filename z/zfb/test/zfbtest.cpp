d22be8d4a4da8aae9e4f763e2588b8f0fe9bc346
Huw Rogers <hrogers@fusionsystems.org>
2019-09-13 10:00:10 +0900

    zfbtest

--- z/zfb/test/zfbtest.cpp
+++ z/zfb/test/zfbtest.cpp
@@ -7,53 +7,6 @@
 using namespace Zfb;
 using namespace zfbtest;
 
-template <typename Vector, typename L>
-inline void push_(Vector &, L &) { }
-template <typename Vector, typename L, typename Arg1, typename ...Args>
-inline void push_(Vector &v, L &l, Arg1 &&arg1, Args &&... args) {
-  v.push_back(l(ZuFwd<Arg1>(arg1)));
-  push_(v, l, ZuFwd<Args>(args)...);
-}
-template <typename T, typename B, typename L, typename ...Args>
-inline auto vector_(B &b, L l, Args &&... args) {
-  std::vector<Offset<T> > v;
-  push_(v, l, ZuFwd<Args>(args)...);
-  return b.CreateVector(v);
-}
-template <typename T, typename B, typename ...Args>
-inline auto vector(B &b, Args &&... args) {
-  return vector_<T>(b, [](auto p) { return p; }, ZuFwd<Args>(args)...);
-}
-template <typename T, typename B, typename L, typename ...Args>
-inline auto keyVec_(B &b, L l, Args &&... args) {
-  std::vector<Offset<T> > v;
-  push_(v, l, ZuFwd<Args>(args)...);
-  return b.CreateVectorOfSortedTables(&v);
-}
-template <typename T, typename B, typename ...Args>
-inline auto keyVec(B &b, Args &&... args) {
-  return keyVec_<T>(b, [](auto p) { return p; }, ZuFwd<Args>(args)...);
-}
-template <typename B, typename S>
-inline auto str(B &b, S &&s) {
-  return b.CreateString(ZuFwd<S>(s));
-}
-template <typename B, typename ...Args>
-inline auto strVec(B &b, Args &&... args) {
-  return vector_<String>(b, [&b](const auto &s) {
-    return str(b, s);
-  }, ZuFwd<Args>(args)...);
-}
-template <typename B>
-inline auto byteVec(B &b, const void *data, unsigned len) {
-  return b.CreateVector(static_cast<const uint8_t *>(data), len);
-}
-template <typename B>
-inline auto byteVec(B &b, ZuString s) {
-  return b.CreateVector(
-      reinterpret_cast<const uint8_t *>(s.data()), s.length());
-}
-
 int main()
 {
   ZmRef<IOBuf> iobuf;
@@ -61,28 +14,30 @@ int main()
   {
     IOBuilder b;
 
-    auto perms = keyVec<Perm>(b,
-	CreatePerm(b, 0, str(b, "useradd")),
-	CreatePerm(b, 1, str(b, "usermod")),
-	CreatePerm(b, 2, str(b, "userdel")),
-	CreatePerm(b, 3, str(b, "login")));
-    uint64_t userPerms = 0x80;
-    uint64_t adminPerms = 0x8f;
-    auto roles = keyVec<Role>(b,
-	CreateRole(b, str(b, "user"), b.CreateVector(&userPerms, 1)),
-	CreateRole(b, str(b, "admin"), b.CreateVector(&adminPerms, 1)));
-    auto users = keyVec<User>(b,
-	CreateUser(b, 0, str(b, "user1"),
-	  byteVec(b, "", 0), byteVec(b, "", 0), strVec(b, "admin")),
-	CreateUser(b, 1, str(b, "user2"),
-	  byteVec(b, "", 0), byteVec(b, "", 0), strVec(b, "user", "admin")));
-    auto keys = keyVec<Key>(b,
-	CreateKey(b, str(b, "1"), byteVec(b, "2", 1), 0),
-	CreateKey(b, str(b, "3"), byteVec(b, "4", 1), 1));
+    {
+      using namespace Zfb::Build;
 
-    auto db = CreateUserDB(b, perms, roles, users, keys);
+      auto perms = keyVec<Perm>(b,
+	  CreatePerm(b, 0, str(b, "useradd")),
+	  CreatePerm(b, 1, str(b, "usermod")),
+	  CreatePerm(b, 2, str(b, "userdel")),
+	  CreatePerm(b, 3, str(b, "login")));
+      auto roles = keyVec<Role>(b,
+	  CreateRole(b, str(b, "user"), pvector<uint64_t>(b, 0x80)),
+	  CreateRole(b, str(b, "admin"), pvector<uint64_t>(b, 0x8f)));
+      auto users = keyVec<User>(b,
+	  CreateUser(b, 0, str(b, "user1"),
+	    bytes(b, "", 0), bytes(b, "", 0), strVec(b, "admin")),
+	  CreateUser(b, 1, str(b, "user2"),
+	    bytes(b, "", 0), bytes(b, "", 0), strVec(b, "user", "admin")));
+      auto keys = keyVec<Key>(b,
+	  CreateKey(b, str(b, "1"), bytes(b, "2", 1), 0),
+	  CreateKey(b, str(b, "3"), bytes(b, "4", 1), 1));
 
-    b.Finish(db);
+      auto db = CreateUserDB(b, perms, roles, users, keys);
+
+      b.Finish(db);
+    }
 
     uint8_t *buf = b.GetBufferPointer();
     int len = b.GetSize();
@@ -100,18 +55,22 @@ int main()
     }
   }
 
-  auto db = GetUserDB(iobuf->data + iobuf->skip);
+  {
+    using namespace Parse;
 
-  auto perm = db->perms()->LookupByKey(1);
+    auto db = GetUserDB(iobuf->data + iobuf->skip);
 
-  if (!perm) {
-    std::cerr << "FAILED - key lookup failed\n" << std::flush;
-    return 1;
-  }
+    auto perm = db->perms()->LookupByKey(1);
 
-  if (string(perm->name()) != "usermod") {
-    std::cerr << "FAILED - wrong key\n" << std::flush;
-    return 1;
+    if (!perm) {
+      std::cerr << "FAILED - key lookup failed\n" << std::flush;
+      return 1;
+    }
+
+    if (str(perm->name()) != "usermod") {
+      std::cerr << "FAILED - wrong key\n" << std::flush;
+      return 1;
+    }
   }
 
   return 0;

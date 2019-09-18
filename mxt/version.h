a2ea03cf371518333b80d26d67222a7aafdad7d2
Huw Rogers <hrogers@fusionsystems.org>
2019-09-18 11:00:43 +0900

    version increments

--- mxt/version.h
+++ mxt/version.h
@@ -24,9 +24,9 @@
 	 (((unsigned long)(minor))<<16) | \
 	 ((unsigned long)(build)))
 
-#define MXT_VERSION MXT_VULONG(2,5,0)	// <-- manually maintained
-#define MXT_VERNAME "2.5.0"		// <-- ''
-#define MXT_VERMSRC 2,5,0,0		// <-- ''
+#define MXT_VERSION MXT_VULONG(2,6,0)	// <-- manually maintained
+#define MXT_VERNAME "2.6.0"		// <-- ''
+#define MXT_VERMSRC 2,6,0,0		// <-- ''
 
 #define MXT_VMAJOR(n) (((unsigned long)n)>>24)
 #define MXT_VMINOR(n) ((((unsigned long)n)>>16) & 0xff)

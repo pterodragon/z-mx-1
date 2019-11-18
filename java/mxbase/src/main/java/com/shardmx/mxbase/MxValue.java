b2919b587d2195bc7d5c4de27600011b554a726f
Huw Rogers <hrogers@fusionsystems.org>
2019-11-18 13:16:14 +0800

    MxDecimal.java

--- java/mxbase/src/main/java/com/shardmx/mxbase/MxValue.java
+++ java/mxbase/src/main/java/com/shardmx/mxbase/MxValue.java
@@ -1,42 +1,19 @@
 package com.shardmx.mxbase;
 
-import java.math.BigDecimal;
-import java.text.DecimalFormat;
-
 public class MxValue {
-  public MxValue(long h, long l) { this.h = h; this.l = l; }
+  public static final long NULL  = Long.MIN_VALUE;
+  public static final long MIN   =  -999999999999999999L;
+  public static final long MAX   =   999999999999999999L;
+  public static final long RESET = -1000000000000000000L; // reset to NULL
 
-  public MxValue(String s) { scan(s); }
-
-  // methods
-
-  // FIXME - need isNull(), isReset(), etc.
-  // FIXME - add(), sub(), div(), mul()
-
-  public static final MxValue valueOf(BigDecimal bd) {
-    BigInteger bi = bd.setScale(18).unscaledValue();
-    long zero = 0;
-    BigInteger mask = BigInteger.valueOf(~zero);
-    return new MxValue(
-      bi.shiftRight(64).longValue(),
-      bi.and(mask).longValue());
+  public Long l;
+  
+  public MxValue(long value) { l = new Long(value); }
+  public boolean equals(MxValue v) { return l.equals(v.l); };
+  public int compareTo(MxValue v) { return l.compareTo(v.l); }
+  public String toString() {
+    if (l.longValue() == NULL) return "";
+    if (l.longValue() == RESET) return "reset";
+    return l.toString();
   }
-
-  public BigDecimal bdValue() {
-    BigInteger bi = BigInteger.valueOf(this.h);
-    bi = bi.shiftLeft(64);
-    bi = bi.or(BigInteger.valueOf(this.l));
-    return new BigDecimal(bi, 18);
-  }
-
-  public native String toString();
-  private native void scan(String s);
-
-  public native boolean equals(MxValue v);
-  public native int compareTo(MxValue v);
-
-  // data members
-
-  private long h;	// high 64 bits
-  private long l;	// low 64 bits
 }
